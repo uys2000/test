a34b92c2d6a63edd2f76c832bab7729aac2a884f
Mehmet Uysal <mehmetuysal2000@yandex.com>
2024-06-07 20:22:27 +0300

    test

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,10 +3,8 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.kirkdokuz.haber49",
   appName: "haber49",
-  webDir: "dist",
   server: {
     url: "https://www.haber49.net/",
-    cleartext: true,
   },
 };
 
